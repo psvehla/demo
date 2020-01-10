87537f9b6b71c188d376b68d45fa3183b40ddde3
peter <psvehla@hotmail.com>
2020-01-11 00:01:47 +1100

    Initial commit.

---
+++ src/test/java/au/com/redbarn/swipejobs/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.redbarn.swipejobs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
